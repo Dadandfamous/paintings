1118fc4f5d5261e30417c39913971d8ebe34c7ed
“Martin <stuurmartipost@gmail.com>
2019-03-19 12:15:36 +0100

    made the page look less terrible

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,11 @@
 function submitComment () {
   console.log('Hello')
+  let inputField = document.getElementById('name')
+  let name = inputField.value
+  let textArea = document.getElementById('msg')
+  let msg = textArea.value
+  let comment = document.createElement('section')
+  // let h3 = document.createElement('<h3>')
+  // console.log(msg)
+  // console.log(h3)
 }
