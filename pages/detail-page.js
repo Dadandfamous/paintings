84ac1f039ccf1316f14d6ae5286d1b87c8df075f
Martin <stuurmartinpost@gmail.com>
2019-03-20 15:01:15 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,6 +6,14 @@ function submitComment () {
   let msg = textArea.value
   let comment = document.createElement('section')
   let h3 = document.createElement('<h3>')
-  console.log(msg)
-  console.log(h3)
+  let p = document.createElement('<p>')
+  
+  // assigning innerhtml:
+
+  innerHTML.h3 = `${name} said:`
+  innerHTML.p = msg
+
+  comment.classList.add('comment')
+  comment.appendChild(h3)
+  comment.appendChild(p)
 }
